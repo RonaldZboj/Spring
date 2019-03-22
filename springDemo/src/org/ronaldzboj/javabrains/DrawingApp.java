a36fa78c4237450f55e5a3fa41545fbc3bc64e19
RonaldZboj <ronaldzboj@gmail.com>
2019-03-22 18:24:05 +0100

    Add Circle and Shape

--- springDemo/src/org/ronaldzboj/javabrains/DrawingApp.java
+++ springDemo/src/org/ronaldzboj/javabrains/DrawingApp.java
@@ -1,6 +1,7 @@
 package org.ronaldzboj.javabrains;
 
-import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
@@ -9,9 +10,14 @@ public class DrawingApp {
 		//Triangle triangle = new Triangle();
 		
 	
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		context.registerShutdownHook();
 		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
+		Circle circle = (Circle) context.getBean("circle");
+		circle.draw();
+		Shape shape = (Shape) context.getBean("circle");
+		shape.draw();
 
 	}
 
--- springDemo/src/org/ronaldzboj/javabrains/Triangle.java
+++ springDemo/src/org/ronaldzboj/javabrains/Triangle.java
@@ -1,13 +1,14 @@
 package org.ronaldzboj.javabrains;
 
-
-public class Triangle {
+public class Triangle implements Shape {
 	
 	private Point pointA;
 	private Point pointB;
 	private Point pointC;
+
 	
 	public void draw() {
+		System.out.println("Drawing Triangle");
 		System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
 		System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
 		System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
@@ -38,7 +39,23 @@ public class Triangle {
 		this.pointC = pointC;
 	}
 
-
-
+	/*
+	 * @Override public void afterPropertiesSet() throws Exception {
+	 * System.out.println("InitializingBean called from Triangle");
+	 * 
+	 * }
+	 * 
+	 * @Override public void destroy() throws Exception {
+	 * System.out.println("DisposibleBean called from Triangle");
+	 * 
+	 * }
+	 */
+	public void myInit() {
+		System.out.println("myInit called from Triangle");
+	}
+	
+	public void cleanUp() {
+		System.out.println("cleanUp called from Triangle");
+	}
 
 }
